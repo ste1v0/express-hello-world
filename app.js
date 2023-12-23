885da37ce1abc5fb3106e0047050d91d3969fcfd
ste1v0 <109760126+ste1v0@users.noreply.github.com>
2023-12-23 18:20:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const request = require('request');
-const app = express();
-const port = process.env.PORT || 3001;
-
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const express = require('express')
+const request = require('request')
+const app = express()
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   next()
 })
-app.get('/goha', (req, res) => {
+
+app.get('/rss', (req, res) => {
   request(
     { url: 'https://www.goha.ru/rss/news' },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: error.message })
+        return res.status(500).json({ type: 'error', message: err.message })
       }
-    
       res.set('Content-Type', 'application/rss+xml')
       res.send(Buffer.from(body))
-    })
+    }
+  )
 })
 
+const PORT = process.env.PORT || 4050
+app.listen(PORT, () => console.log(`listening on ${PORT}`))
 
 
 
